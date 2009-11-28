d2df381ab1961745ecfdb280fd83f079d920929d
Maciej Konieczny <hello@narf.pl>
2009-11-29 00:58:16 +0100

    added handling of looper special first "+" parameter, refactored variable names

--- .scripts/looper/looper.py
+++ .scripts/looper/looper.py
@@ -3,6 +3,8 @@
 """
 Base script for automatically running commands on file modification.
 
+No pretty errors for executing it with not enough parameters.
+
 example:
 
     $ alias loop="python sth_that_imports_looper.py"  # see: loop_python.py
@@ -11,17 +13,25 @@ example:
     => tracked_files = ['myfile']
        main_file = 'myfile'
        args = ''
+       passed_special_parameter = False
+
+    $ loop + myfile
+    => tracked_files = ['myfile']
+       main_file = 'myfile'
+       args = ''
+       passed_special_parameter = True
 
     $ loop `find . -name "*.py"` foo.py -my args
     => tracked_files = ['./foo.py', './bar.py', 'foo.py']
        main_file = 'foo.py'
        args = '-my args'
+       passed_special_parameter = False
 
 """
 
 import os
 import stat
-from sys import argv
+import sys
 from time import sleep
 
 
@@ -30,21 +40,30 @@ __author__ = 'Maciej Konieczny <hello@narf.pl>'
 
 class LoopParameters(object):
     """
-    Attributes: args, main_file, tracked_files.
+    Attributes: args, main_file, tracked_files, passed_special_parameter.
     """
 
     def __init__(self):
+        parameters = sys.argv[1:]
+
+        # check if first parameter is a plus
+        if parameters[0] == '+':
+            self.passed_special_parameter = True
+            parameters = parameters[1:]
+        else:
+            self.passed_special_parameter = False
+
         # get args
-        for i, word in enumerate(argv):
+        for i, word in enumerate(parameters):
             if word.startswith('-'):
-                self.args = ' '.join(argv[i:])
+                self.args = ' '.join(parameters[i:])
                 break
         else:  # if no break
             self.args = ''
             i += 1
 
         # get tracked files and main file paths
-        self.tracked_files = argv[1:i]
+        self.tracked_files = parameters[:i]
         self.main_file = self.tracked_files[-1]
 
 
