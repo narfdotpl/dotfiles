fb062391e8eec49ab29adcbb737c734f69ee4058
Maciej Konieczny <hello@narf.pl>
2009-12-15 00:43:07 +0100

    added default loop parameters

--- .scripts/loops/loop/loop.py
+++ .scripts/loops/loop/loop.py
@@ -4,10 +4,38 @@
 Run command on file modification.
 """
 
+import sys
+
 
 __author__ = 'Maciej Konieczny <hello@narf.pl>'
 
 
 class Loop(object):
 
-    pass
+    def __init__(self, parameters=sys.argv[1:]):
+        # be pesimistic
+        self.passed_special_parameter = False
+        self.tracked_files = None
+        self.main_file = None
+        self.args = None
+
+        # special parameter
+        if parameters and parameters[0] == '+':
+            self.passed_special_parameter = True
+            parameters = parameters[1:]
+
+        # quit if there is nothing left to parse
+        if not parameters:
+            return
+
+        # args
+        for i, parameter in enumerate(parameters):
+            if parameter.startswith('-'):
+                self.args = ' '.join(parameters[i:])
+                break
+        else:  # if no break
+            i += 1
+
+        # tracked files and main file
+        self.tracked_files = parameters[:i]
+        self.main_file = self.tracked_files[-1]
--- .scripts/loops/loop/tests.py
+++ .scripts/loops/loop/tests.py
@@ -8,3 +8,34 @@ Run with nose http://somethingaboutorange.com/mrl/projects/nose
 from nose.tools import assert_equals
 
 from loop import Loop
+
+
+class TestDefaultParameters:
+
+    def test_get_all_clean_on_clean_init(self):
+        loop = Loop(parameters=[])
+        for actual, expected in [
+            (loop.passed_special_parameter, False),
+            (loop.tracked_files, None),
+            (loop.main_file, None),
+            (loop.args, None),
+        ]:
+            assert_equals(actual, expected)
+
+    def test_get_all_properly_parsed(self):
+        # prepare parameters
+        parameters = ['+']  # passed_special_parameter = True
+        tracked_files = ['foo', 'bar']
+        parameters.extend(tracked_files)
+        main_file = tracked_files[-1]
+        args = '-3 --verbose reset --hard'
+        parameters.extend(args.split())
+
+        loop = Loop(parameters=parameters)
+        for actual, expected in [
+            (loop.passed_special_parameter, True),
+            (loop.tracked_files, tracked_files),
+            (loop.main_file, main_file),
+            (loop.args, args),
+        ]:
+            assert_equals(actual, expected)
