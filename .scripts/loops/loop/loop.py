5f0aa30aa1cf289f0b1ba4d45f91ceaa0649841e
Maciej Konieczny <hello@narf.pl>
2009-12-14 23:39:51 +0100

    added initial files for new loop infrastructure

---
+++ .scripts/loops/loop/loop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Run command on file modification.
+"""
+
+
+__author__ = 'Maciej Konieczny <hello@narf.pl>'
+
+
+class Loop(object):
+
+    pass
---
+++ .scripts/loops/loop/tests.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+"""
+Test suite.
+
+Run with nose http://somethingaboutorange.com/mrl/projects/nose
+"""
+
+from nose.tools import assert_equals
+
+from loop import Loop
