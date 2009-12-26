1055b1e7b74da51aa55a5240c430a320aad8da75
Maciej Konieczny <hello@narf.pl>
2009-12-26 01:01:58 +0100

    added not empty stash list indicator to git prompt

--- .scripts/git/prompt.py
+++ .scripts/git/prompt.py
@@ -4,17 +4,21 @@
 Parse Git information for shell prompt.
 
 Show current branch name.  If there were any changes since last commit, show
-a plus sign.  If current directory is not the top-level directory of the
-repository, precede branch name with one dot for each directory below the top.
+a plus sign.  If there are any stashed changes, show an ampersand.  If current
+directory is not the top-level directory of the repository, precede branch name
+with one dot for each directory below the top.
 
 example:
 
     ~ $ cd dotfiles
     ~/dotfiles(master) $ cd .scripts
     ~/dotfiles/.scripts(.master) $ touch foo
-    ~/dotfiles/.scripts(.master+) $
+    ~/dotfiles/.scripts(.master+) $ git add foo; git stash
+    ~/dotfiles/.scripts(.master&) $
 
 """
+
+from subprocess import PIPE, Popen
 
 from git import Git
 
@@ -28,10 +32,20 @@ def _main():
     except EnvironmentError:
         pass
     else:
+        # depth
         prompt = '.' * git.depth
+
+        # branch name
         prompt += git.branch
+
+        # changes
         if not git.is_clean:
             prompt += '+'
+
+        # stashed
+        if Popen(['git', 'stash', 'list'], stdout=PIPE).stdout.readlines():
+            prompt += '&'
+
         print '(' + prompt + ')',
 
 if __name__ == '__main__':
