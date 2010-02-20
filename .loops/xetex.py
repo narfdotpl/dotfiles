f61d7b4d856cf0d362e44d2b21a8662b81413f83
Maciej Konieczny <hello@narf.pl>
2010-02-20 10:39:31 +0100

    removed the rm part from XeTeX loop

--- .loops/xetex.py
+++ .loops/xetex.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-Compile XeTeX file and remove everything but source and the pdf.
+Compile XeTeX.
 """
 
 from loopozorg import Loop
 
 
 def _main():
-    loop = Loop()
-    loop.aux = loop.bin + '.aux'
-    loop.log = loop.bin + '.log'
-    loop.run('xelatex {main_file};'
-             '[[ -f {aux} ]] && rm {aux};'
-             '[[ -f {log} ]] && rm {log}')
+    Loop('xelatex {main_file}')
 
 if __name__ == '__main__':
     _main()
