f59c775866d3be0072846536a2ea398f30844bd7
Maciej Konieczny <hello@narf.pl>
2010-04-08 11:23:10 +0200

    added git push-upto script

---
+++ .scripts/git/push-upto.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Push changes to origin up to specific commit.
+
+Usage:
+
+    python push-upto.py sha [push options]
+
+"""
+
+from subprocess import call
+from sys import argv, stderr
+
+from git import Git
+
+
+__author__ = 'Maciej Konieczny <hello@narf.pl>'
+
+
+def _main():
+    git = Git()
+
+    if len(argv) < 2:
+        print >> stderr, __doc__[1:-1]
+        exit(1)
+
+    call('git push {options} origin {sha}:{branch}'.format(
+        sha=argv[1],
+        options=' '.join(argv[2:]),
+        branch=git.branch
+    ), shell=True)
+
+if __name__ == '__main__':
+    _main()
