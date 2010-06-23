54a54247ef671f8edb8365f420866e3ca342bab4
Maciej Konieczny <hello@narf.pl>
2010-06-23 10:15:08 +0200

    added "update branch" git script

---
+++ home/.scripts/git/update-branch.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Pull master (and rebase current branch against it).
+
+Usage:
+
+    python update-branch.py
+
+"""
+
+from subprocess import call
+from sys import argv, stderr
+
+from git import Git
+
+
+__author__ = 'Maciej Konieczny <hello@narf.pl>'
+
+
+def system(command):
+    return call(command, shell=True)
+
+
+def _main():
+    # stop if there's no repo
+    git = Git()
+
+    # show usage info if there are too many arguments
+    if len(argv) > 1:
+        print >> stderr, __doc__[1:-1]
+        exit(1)
+
+    # do the magic
+    branch = git.branch
+    if branch == 'master':
+        system('git pull')
+    else:
+        system('git checkout master')
+        system('git pull')
+        system('git checkout ' + branch)
+        system('git rebase master')
+
+
+if __name__ == '__main__':
+    _main()
