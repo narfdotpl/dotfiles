fc6c737aa289faa2d845e2c0753a84dd04011159
Maciej Konieczny <hello@narf.pl>
2011-03-21 16:31:26 +0100

    resurrected octave loop

--- home/.loops/aliases.zsh
+++ home/.loops/aliases.zsh
@@ -3,6 +3,7 @@ alias lcs='loop c-sharp'
 alias lm='loop markdown'
 alias lno-='loop nose `find . -name "*.py"`'
 lno() {lno- $@ --cover-package=$(basename $(pwd))}
+alias lo='loop "octave --silent {main_file} {args}"'
 alias lp='loop "python -3 {main_file} {args}"'
 alias lpy='loop "python {main_file} {args}"'
 alias lp3='loop "python3 {main_file} {args}"'
