154030a8f5a0405a01a816008816441f44198114
Maciej Konieczny <hello@narf.pl>
2021-11-23 14:34:07 +0100

    added fzf key bindings and auto-completion

--- home/.fzf.zsh
+++ home/.fzf.zsh
@@ -5,3 +5,10 @@
 
 export FZF_DEFAULT_COMMAND="ag -l '.'"
 export FZF_DEFAULT_OPTS="--reverse"
+
+
+# auto-completion
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# key bindings
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
